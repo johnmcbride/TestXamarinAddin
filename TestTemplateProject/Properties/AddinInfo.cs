e27e37a4d691b49caacbbe417d6250d3693ac74f
John McBride <john@johnmcbride.me>
2015-02-22 22:57:55 -0500

    test project

---
+++ TestTemplateProject/Properties/AddinInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"TestTemplateProject", 
+	Namespace = "TestTemplateProject",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("TestTemplateProject")]
+[assembly:AddinCategory ("TestTemplateProject")]
+[assembly:AddinDescription ("TestTemplateProject")]
+[assembly:AddinAuthor ("john")]
+
+[assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
+
+
